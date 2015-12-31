9a39294840ea47595036e2939eba05121c8ee7ae
ztelur <ztelur@gmail.com>
2015-12-31 22:56:35 +0800

    git clone [url,url,]

--- pygit.py
+++ pygit.py
@@ -2,31 +2,36 @@
 import sys
 import getopt
 import os
-def git = 'git'
-def pull_cmd = 'pull'
-def checkout_cmd = 'pull'
-def branch_cmd = 'branch'
-def status_cmd = 'status'
-def clone_cmd = 'clone'
+git = 'git'
+pull_cmd = 'pull'
+checkout_cmd = 'pull'
+branch_cmd = 'branch'
+status_cmd = 'status'
+clone_cmd = 'clone'
 def main(argv):
+	print str(argv)
 	if len(argv) <=1:
 		return
+	cmd = argv[0]
 	if isPull(cmd):
-
-
+		pull()
+	if isClone(cmd):
+		clone(list(argv[1:]))
+def isClone(str):
+	return 'clone' == str.lower()	
 def isPull(str):
-	return cmp('pull',str.lower())
-
+	return 'pull'==str.lower()
 def pull():
 	print "pull"
 def checkout():
 	print "checkout"
-
 def clone(init_url):
-	for(url:init_url):
+	print init_url;
+	for url in init_url:
 		execute_cmd = git+" "+clone_cmd+" "+url
 		print "execute "+execute_cmd
 		os.system(execute_cmd)
 
-
+if __name__ == "__main__":
+   main(sys.argv[1:])
 
