13e7c081b7906605fb6a46bd7376ee7a2845d3ff
ztelur <ztelur@gmail.com>
2016-01-01 11:28:03 +0800

    checkout for all git repo

--- pygit.py
+++ pygit.py
@@ -4,10 +4,11 @@ import getopt
 import os
 git = 'git'
 pull_cmd = 'pull'
-checkout_cmd = 'pull'
+checkout_cmd = 'checkout'
 branch_cmd = 'branch'
 status_cmd = 'status'
 clone_cmd = 'clone'
+git_dir = ".git"
 def main(argv):
 	print str(argv)
 	if len(argv) <=1:
@@ -17,14 +18,28 @@ def main(argv):
 		pull()
 	if isClone(cmd):
 		clone(list(argv[1:]))
+	if isCheckout(cmd):
+		checkout(argv[1])
 def isClone(str):
 	return 'clone' == str.lower()	
 def isPull(str):
-	return 'pull'==str.lower()
+	return 'pull' == str.lower()
+def isCheckout(str):
+	return "checkout" == str.lower()
 def pull():
 	print "pull"
-def checkout():
-	print "checkout"
+def checkout(branchs):
+	cwd = os.getcwd()
+	for repo in os.listdir(cwd):
+		if os.path.isdir(repo):
+			print "cd "+ repo
+			os.chdir(repo)
+			if (os.path.exists(git_dir)):
+				execute_cmd = git + " " + checkout_cmd + " " + branchs
+				print repo + " " + execute_cmd
+				os.system(execute_cmd)
+			os.chdir(cwd);
+	print "checkout finish"
 def clone(init_url):
 	print init_url;
 	for url in init_url:
